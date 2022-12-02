27cb3299bdab654cc444200d39acdd27f44563e4
alitdarmaputra <74844470+alitdarmaputra@users.noreply.github.com>
2022-12-03 01:05:58 +0800

    Add local capabilities lsp-config
    Fix html lsp not working

--- nvim/lua/lsp-config/init.lua
+++ nvim/lua/lsp-config/init.lua
@@ -1,3 +1,6 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 require("lspconfig")["pyright"].setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
